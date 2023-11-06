ab711a63011cab0b36191bf88a026bc8a3aea2ed
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2023-11-05 22:15:16 -0600

    gitnore, instalacion de yfinance

--- inviertec_api/enterprises/views.py
+++ inviertec_api/enterprises/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+import yfinance as yf
+from datetime import date, timedelta
+
+@api_view(['GET'])
+def obtener_datos_empresa(request, nombre_empresa):
+    try:
+        ticker = yf.Ticker(nombre_empresa)
+        today = date.today()
+        start_date = today - timedelta(days=365 * 2)
+        data = ticker.history(start=start_date, end=today)
+        data = data.reset_index()
+        data = data[['Date', 'Adj Close']]
+        data.columns = ['fecha', 'precio']
+        data = data.to_dict(orient='records')
+
+        return Response(data)
+    except Exception as e:
+        return Response({'error': str(e)})
