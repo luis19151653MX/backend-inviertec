f6b389f70f7fcdee9c11446d2b1994a7aeec97b1
CassandraCHZ <19151765@aguascalientes.tecnm.mx>
2023-11-08 19:21:57 -0600

    ya funciona la api, permite cors

--- inviertec_api/inviertec_api/settings.py
+++ inviertec_api/inviertec_api/settings.py
@@ -38,7 +38,25 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 ]
+
+CORS_ALLOW_ALL_ORIGINS=True
+
+"""
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3002",
+]
+
+CORS_ALLOW_HEADERS = (
+    "accept",
+    "authorization",
+    "content-type",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+)
+"""
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'inviertec_api.urls'
