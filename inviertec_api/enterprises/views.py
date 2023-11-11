11d53b5d7c8b4b3b0a981dfe9a2015fc3ae7b236
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2023-11-10 23:04:51 -0600

    imagenes de empresas

--- inviertec_api/enterprises/views.py
+++ inviertec_api/enterprises/views.py
@@ -7,26 +7,26 @@ import yfinance as yf
 from datetime import date, timedelta
 
 empresas = [
-    {'idEnterprise': 1, 'nameEnterprise': 'Apple Inc.', 'ticker': 'AAPL'},
-    {'idEnterprise': 2, 'nameEnterprise': 'Microsoft Corporation', 'ticker': 'MSFT'},
-    {'idEnterprise': 3, 'nameEnterprise': 'Amazon.com Inc.', 'ticker': 'AMZN'},
-    {'idEnterprise': 4, 'nameEnterprise': 'Alphabet Inc.', 'ticker': 'GOOGL'},
-    {'idEnterprise': 5, 'nameEnterprise': 'Meta Platforms, Inc.', 'ticker': 'META'},
-    {'idEnterprise': 6, 'nameEnterprise': 'Tesla, Inc.', 'ticker': 'TSLA'},
-    {'idEnterprise': 7, 'nameEnterprise': 'Johnson & Johnson', 'ticker': 'JNJ'},
-    {'idEnterprise': 8, 'nameEnterprise': 'Procter & Gamble Co.', 'ticker': 'PG'},
-    {'idEnterprise': 9, 'nameEnterprise': 'JPMorgan Chase & Co.', 'ticker': 'JPM'},
-    {'idEnterprise': 10, 'nameEnterprise': 'Goldman Sachs Group, Inc.', 'ticker': 'GS'},
-    {'idEnterprise': 11, 'nameEnterprise': 'Exxon Mobil Corporation', 'ticker': 'XOM'},
-    {'idEnterprise': 12, 'nameEnterprise': 'Chevron Corporation', 'ticker': 'CVX'},
-    {'idEnterprise': 13, 'nameEnterprise': 'Pfizer Inc.', 'ticker': 'PFE'},
-    {'idEnterprise': 14, 'nameEnterprise': 'The Coca-Cola Company', 'ticker': 'KO'},
-    {'idEnterprise': 15, 'nameEnterprise': 'Intel Corporation', 'ticker': 'INTC'},
-    {'idEnterprise': 16, 'nameEnterprise': 'Boeing Company', 'ticker': 'BA'},
-    {'idEnterprise': 17, 'nameEnterprise': 'Walt Disney Company', 'ticker': 'DIS'},
-    {'idEnterprise': 18, 'nameEnterprise': 'IBM', 'ticker': 'IBM'},
-    {'idEnterprise': 19, 'nameEnterprise': 'Visa Inc.', 'ticker': 'V'},
-    {'idEnterprise': 20, 'nameEnterprise': 'Cisco Systems, Inc.', 'ticker': 'CSCO'},
+    {'idEnterprise': 1, 'nameEnterprise': 'Apple Inc.', 'ticker': 'AAPL', 'image': 'http://127.0.0.1:3002/images/enterprises/aapl.png'},
+    {'idEnterprise': 2, 'nameEnterprise': 'Microsoft Corporation', 'ticker': 'MSFT', 'image': 'http://127.0.0.1:3002/images/enterprises/msft.png'},
+    {'idEnterprise': 3, 'nameEnterprise': 'Amazon.com Inc.', 'ticker': 'AMZN', 'image': 'http://127.0.0.1:3002/images/enterprises/amzn.png'},
+    {'idEnterprise': 4, 'nameEnterprise': 'Alphabet Inc.', 'ticker': 'GOOGL', 'image': 'http://127.0.0.1:3002/images/enterprises/googl.png'},
+    {'idEnterprise': 5, 'nameEnterprise': 'Meta Platforms, Inc.', 'ticker': 'META', 'image': 'http://127.0.0.1:3002/images/enterprises/meta.png'},
+    {'idEnterprise': 6, 'nameEnterprise': 'Tesla, Inc.', 'ticker': 'TSLA', 'image': 'http://127.0.0.1:3002/images/enterprises/tsla.png'},
+    {'idEnterprise': 7, 'nameEnterprise': 'Johnson & Johnson', 'ticker': 'JNJ', 'image': 'http://127.0.0.1:3002/images/enterprises/jnj.png'},
+    {'idEnterprise': 8, 'nameEnterprise': 'Procter & Gamble Co.', 'ticker': 'PG', 'image': 'http://127.0.0.1:3002/images/enterprises/pg.png'},
+    {'idEnterprise': 9, 'nameEnterprise': 'JPMorgan Chase & Co.', 'ticker': 'JPM', 'image': 'http://127.0.0.1:3002/images/enterprises/jpm.png'},
+    {'idEnterprise': 10, 'nameEnterprise': 'Starbucks Corporation', 'ticker': 'SBUX', 'image': 'http://127.0.0.1:3002/images/enterprises/sbux.png'},
+    {'idEnterprise': 11, 'nameEnterprise': 'Exxon Mobil Corporation', 'ticker': 'XOM', 'image': 'http://127.0.0.1:3002/images/enterprises/xom.png'},
+    {'idEnterprise': 12, 'nameEnterprise': 'Chevron Corporation', 'ticker': 'CVX', 'image': 'http://127.0.0.1:3002/images/enterprises/cvx.png'},
+    {'idEnterprise': 13, 'nameEnterprise': 'Pfizer Inc.', 'ticker': 'PFE', 'image': 'http://127.0.0.1:3002/images/enterprises/pfe.png'},
+    {'idEnterprise': 14, 'nameEnterprise': 'The Coca-Cola Company', 'ticker': 'KO', 'image': 'http://127.0.0.1:3002/images/enterprises/ko.png'},
+    {'idEnterprise': 15, 'nameEnterprise': 'Intel Corporation', 'ticker': 'INTC', 'image': 'http://127.0.0.1:3002/images/enterprises/intc.png'},
+    {'idEnterprise': 16, 'nameEnterprise': 'Boeing Company', 'ticker': 'BA', 'image': 'http://127.0.0.1:3002/images/enterprises/ba.png'},
+    {'idEnterprise': 17, 'nameEnterprise': 'Walt Disney Company', 'ticker': 'DIS', 'image': 'http://127.0.0.1:3002/images/enterprises/dis.png'},
+    {'idEnterprise': 18, 'nameEnterprise': 'IBM', 'ticker': 'IBM', 'image': 'http://127.0.0.1:3002/images/enterprises/ibm.png'},
+    {'idEnterprise': 19, 'nameEnterprise': 'Visa Inc.', 'ticker': 'V', 'image': 'http://127.0.0.1:3002/images/enterprises/v.png'},
+    {'idEnterprise': 20, 'nameEnterprise': 'Cisco Systems, Inc.', 'ticker': 'CSCO', 'image': 'http://127.0.0.1:3002/images/enterprises/csco.png'},
 ]
 
 empresas_datos=[]
@@ -49,7 +49,8 @@ def cargar_datos_empresas():
                 'idEnterprise': empresa['idEnterprise'],
                 'nameEnterprise': empresa['nameEnterprise'],
                 'ticker': empresa['ticker'],
-                'lastPrice': ultimo_precio_cierre
+                'lastPrice': ultimo_precio_cierre,
+                'image':empresa['image']
             }
             empresas_datos.append(resultado_empresa)
             resultados.append(resultado_empresa)
